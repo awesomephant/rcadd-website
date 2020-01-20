5936af0cf183bce5a06db17e9ae24d0d9d887218
Max Kohler <max@koehler-kn.de>
2020-01-20 20:00:10 +0000

    update model

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 let renderer, scene, camera, door, doorWire;
+let last_known_scroll_position = 0;
+let ticking = false;
+let currentBlur = 0;
+let container = null;
+
+function handleScroll(scroll_pos) {
+    currentBlur = (scroll_pos * .05) - 20;
+//    container.style.filter = `blur(${currentBlur}px)`
+}
+
+window.addEventListener('scroll', function (e) {
+    last_known_scroll_position = window.scrollY;
+    if (!ticking) {
+        window.requestAnimationFrame(function () {
+            handleScroll(last_known_scroll_position);
+            ticking = false;
+        });
+        ticking = true;
+    }
+});
 
 function animate() {
     requestAnimationFrame(animate);
-    door.rotation.z += 0.0005;
+    door.rotation.z += 0.0003;
+    doorWire.rotation.z += 0.0003;
     door.rotation.y += 0.0005;
+    doorWire.rotation.y += 0.0005;
 
-//    camera.position.z -= .001;
+    //    camera.position.z -= .001;
     renderer.render(scene, camera);
 };
 
@@ -22,12 +44,12 @@ function dumpObject(obj, lines = [], isLast = true, prefix = '') {
 }
 
 function initHero() {
-    let container = document.querySelector('.home-hero')
+    container = document.querySelector('.home-hero')
     scene = new THREE.Scene();
     const h = (window.innerHeight * 1)
     const w = (window.innerWidth * 1)
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / h, 0.1, 1000);
-    
+
     renderer = new THREE.WebGLRenderer({
         alpha: true
     });
@@ -41,12 +63,13 @@ function initHero() {
         scene.add(root);
         console.log(dumpObject(root).join('\n'));
         door = root.getObjectByName('door');
+        doorWire = root.getObjectByName('doorWire');
         animate();
     }, undefined, function (error) {
         console.error(error);
     });
 
-    camera.position.z = 5;
+    camera.position.z = 3;
 }
 
 
